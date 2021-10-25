cb7001632d66ae3ab00123e6751b943d91d6a560
Rustam Semenov <88612028+SemenovRustam@users.noreply.github.com>
2021-10-25 13:55:33 +0500

    Add files via upload

---
+++ spring_course_rest/src/main/java/com/rustamsemenov/spring/rest/dao/EmployeeDAO.java
@@ -0,0 +1,16 @@
+package com.rustamsemenov.spring.rest.dao;
+
+import com.rustamsemenov.spring.rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    public List<Employee> getAllEmployee();
+
+   public    void saveEmployee(Employee employee);
+
+   public Employee getEmployee(int id);
+
+   public void deleteEmployee(int id);
+}
---
+++ spring_course_rest/src/main/java/com/rustamsemenov/spring/rest/service/EmployeeServiceImpl.java
@@ -0,0 +1,40 @@
+package com.rustamsemenov.spring.rest.service;
+
+import com.rustamsemenov.spring.rest.dao.EmployeeDAO;
+import com.rustamsemenov.spring.rest.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl  implements EmployeeService {
+
+    @Autowired
+    private EmployeeDAO employeeDAO;
+
+    @Override
+    @Transactional
+    public List<Employee> getAllEmployees() {
+        return employeeDAO.getAllEmployee();
+    }
+
+    @Override
+    @Transactional
+    public void saveEmployee(Employee employee) {
+        employeeDAO.saveEmployee(employee);
+    }
+
+    @Transactional
+    @Override
+    public void deleteEmployee(int id) {
+       employeeDAO.deleteEmployee(id);
+    }
+
+    @Override
+    @Transactional
+    public Employee getEmployee(int id) {
+        return employeeDAO.getEmployee(id);
+    }
+}
